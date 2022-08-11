c497a692bb2b9c086abc4a01728aaac457256de8
saadahmed388 <saadahmed388@gmail.com>
2022-08-11 16:38:38 +0530

    detection project

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # logo and num plate repo
- 
+In this project tensorflow object detecting module is used to train the object detection model using RESNET 640 x 640 architecture, the dataset was self collected and was annotated for logo and number plate using labelimg tool
+Used a CNN for car logo brand prediction (logo brand prediction is limited to these 8 brands namely "Hyundai","Lexus","Mercedes","Opel","Skoda","Toyota","Volkswagen","Mazda" due to lack of dataset), the dataset was trained on Xception CNN architecture which is a slightly advanced version of inception V3.Training on this model resulted in a validation accuracy of 86%.
+For the number plate part, easyocr library is used to produce string output by reading the number plate.
+
+Further more, the model for detection on images is deployed using the gradio interface.
+The detection on images can be done using the detection-image.py file and for performing detection using webcam or videofile upload can be done using detection-vid.py file (For the video part, when the frame window opens after running the code, press 'q' for performing the prediction, pressing 'a' breaks the operation).
+
+
+
---
+++ detection-vid.py
@@ -0,0 +1,151 @@
+import os
+from sre_constants import SUCCESS
+import time
+import tensorflow as tf
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as viz_utils
+
+print (tf.__version__)
+
+PATH_TO_SAVED_MODEL = "D:\\Projects\\Detection\\Trained_Models\\loc_model1\\saved_model" #Input the path where the model of detection and localization is
+new_model = tf.keras.models.load_model("D:\\Projects\\Detection\\Trained_Models\\cnn_model") # load the cnn model for logo brand prediction
+print('Loading model...', end='')
+start_time = time.time()
+
+
+detect_fn = tf.saved_model.load(PATH_TO_SAVED_MODEL)
+
+end_time = time.time()
+elapsed_time = end_time - start_time
+print('Done! Took {} seconds'.format(elapsed_time))
+
+category_index = label_map_util.create_category_index_from_labelmap("D:\\Projects\\Detection\\label_map.pbtxt",##Input the path of label_map.pbtxt
+                                                                    use_display_name=True)
+
+import numpy as np
+import tensorflow as tf
+from PIL import Image
+import matplotlib.pyplot as plt
+import warnings
+import cv2
+import easyocr
+
+warnings.filterwarnings('ignore')   
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    success, image_np = cap.read()
+    if success:
+        input_tensor = tf.convert_to_tensor(image_np)
+        input_tensor = input_tensor[tf.newaxis, ...]
+        detections = detect_fn(input_tensor)
+    
+        num_detections = int(detections.pop('num_detections'))
+        detections = {key: value[0, :num_detections].numpy()
+                    for key, value in detections.items()}
+    
+        detections['num_detections'] = num_detections
+        detections['detection_classes'] = detections['detection_classes'].astype(np.int64)
+
+        image_np_with_detections = image_np.copy()
+
+        viz_utils.visualize_boxes_and_labels_on_image_array(
+            image_np_with_detections,
+            detections['detection_boxes'],
+            detections['detection_classes'],
+            detections['detection_scores'],
+            category_index,
+            use_normalized_coordinates=True,
+            max_boxes_to_draw=2,
+            min_score_thresh=.50,
+            agnostic_mode=False)
+
+        cv2.imshow('frame',image_np_with_detections)
+        
+    else:
+        break    
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+                
+        #cv2.imshow("image",image_np)
+        cv2.imwrite('logo.jpg', image_np)
+        flag1 = False
+        flag2 = False
+                        
+        im_height, im_width, im_chan = image_np.shape
+        global text_out
+        global text_out2
+
+        for i in range(5):
+            if detections['detection_classes'][i] == 1 and flag1 == False:
+                
+                ymin,xmin,ymax,xmax = detections['detection_boxes'][i]
+
+                xmin*=im_width
+                xmax*=im_width
+                ymin*=im_height
+                ymax*=im_height
+
+                dim =(256,256)
+                
+                class_names = ['hyundai','lexus','mazda','mercedes','opel','skoda','toyota','volkswagen'] # due to lack of datasets , currently limited to  8 brands only
+                img = cv2.imread('logo.jpg')
+                crop_img = img[round(ymin):round(ymax), round(xmin):round(xmax)]
+                crop_img = crop_img.copy()
+                resized = cv2.resize(crop_img, dim, interpolation = cv2.INTER_AREA)
+                confidence = round((detections['detection_scores'][i]*100),2)
+                #print('Detected logo with ',confidence,'% confidence')
+                
+                
+                resized = np.expand_dims(resized,0)
+                prediction = new_model.predict(resized)
+                confidence = round((np.max(prediction)*100),2)
+                #print('Predicted brand is',class_names[np.argmax(prediction)])
+                #print('with',confidence,'% confidence')  
+                text_out = 'Predicted brand is '+ class_names[np.argmax(prediction)] + ' with ' + str(confidence) + '% confidence....'                                                                  
+                flag1 = True
+                i +=1
+                
+            if detections['detection_classes'][i] == 2 and flag2 == False:
+                ymin,xmin,ymax,xmax = detections['detection_boxes'][i]
+
+                xmin*=im_width
+                xmax*=im_width
+                ymin*=im_height
+                ymax*=im_height
+                dim = (400,100)
+
+                img = cv2.imread('logo.jpg')
+                crop_img = img[round(ymin):round(ymax), round(xmin):round(xmax)]
+                crop_img = crop_img.copy()
+                resized1 = cv2.resize(crop_img, dim, interpolation = cv2.INTER_AREA)
+                
+                
+                
+                reader = easyocr.Reader(['en'])
+                ocr_result = reader.readtext(resized1)
+                list = []
+                for result in ocr_result:
+                    length = np.sum(np.subtract(result[0][1],result[0][0]))
+                    width = np.sum(np.subtract(result[0][2],result[0][1]))
+                    threshold = (length*width)/30000
+                    if(threshold>0.3): list.append(result[1])
+                str1 = " "   
+                str1 = str1.join(list)    
+                text_out2 = 'Recognized license registration number is : ' + str1
+                
+                confidence1 = round((detections['detection_scores'][i]*100),2)
+                flag2 = True
+                
+            if flag1 == True and flag2 == True:
+               break
+        print(text_out+'\n'+text_out2)       
+
+    elif cv2.waitKey(1) & 0xFF == ord('a'):
+        break  
+        
+        
+    
+cap.release()
+cv2.destroyAllWindows()  
